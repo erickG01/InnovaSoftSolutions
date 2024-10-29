a4d7b8f93835490d82ef97c684f5ccc71425c671
Eleaza-R <otoeleazarivas@gmail.com>
2024-10-28 22:44:25 -0600

    Se arreglo la tabla

--- App_innovaSoft/views.py
+++ App_innovaSoft/views.py
@@ -14,6 +14,10 @@ def libroMayor(request):
 #Vistas CatalogoCuentas
 
 
+
+
+from django.core.paginator import Paginator
+
 def tipos_cuentas(request):
     # Obtener los filtros del request
     tipo_cuenta_id = request.GET.get('tipoCuenta')
@@ -26,24 +30,29 @@ def tipos_cuentas(request):
     cuentas_mayor = CuentaDeMayor.objects.all()
 
     # Filtrar las subcuentas según los filtros seleccionados
-    subcuentas = SubCuenta.objects.all()  # Cambia esto al modelo correcto de las cuentas
+    subcuentas = SubCuenta.objects.select_related('idDeMayor__idRubro__idGrupoCuenta').prefetch_related('detalles')
 
-    # Filtrado condicional
-    if tipo_cuenta_id and tipo_cuenta_id != '':
+    # Aplicar filtros si están presentes
+    if tipo_cuenta_id:
         subcuentas = subcuentas.filter(idDeMayor__idRubro__idGrupoCuenta=tipo_cuenta_id)
 
-    if rubro_id and rubro_id != '':
+    if rubro_id:
         subcuentas = subcuentas.filter(idDeMayor__idRubro__idRubro=rubro_id)
 
-    if cuenta_mayor_id and cuenta_mayor_id != '':
+    if cuenta_mayor_id:
         subcuentas = subcuentas.filter(idDeMayor__idDeMayor=cuenta_mayor_id)
 
-    # Agregar las subcuentas filtradas al contexto
+    # Paginación
+    paginator = Paginator(subcuentas, 10)  # Mostrar 10 subcuentas por página
+    page_number = request.GET.get('page')  # Número de página actual
+    page_obj = paginator.get_page(page_number)  # Obtener la página
+
+    # Agregar las subcuentas paginadas al contexto
     context = {
         'tipos_cuenta': tipos_cuenta,
         'rubros': rubros,
         'cuentas_mayor': cuentas_mayor,
-        'subcuentas': subcuentas,  # Incluye las subcuentas filtradas
+        'page_obj': page_obj,  # Usar page_obj en la plantilla
     }
 
     return render(request, 'App_innovaSoft/CatalogoCuentas.html', context)
